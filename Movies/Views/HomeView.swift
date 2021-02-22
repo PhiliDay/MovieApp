c455d16c10dcc0c08a14307dd220be31be85a504
Phili Day <philippa.day@tigerspike.com>
2021-02-22 21:25:26 +0000

    navigation and child

--- Movies/Views/HomeView.swift
+++ Movies/Views/HomeView.swift
@@ -9,20 +9,38 @@ import SwiftUI
 
 struct HomeView: View {
     @State var movies: [Movie] = []
+    @State private var tapped: Bool = false
 
     var body: some View {
-        List(movies) { movie in
-            HStack {
-                ImageView(withURL: movie.image)
-                VStack(alignment: .leading) {
-                    Text(movie.fullTitle)
-                    Text("Rating: \(movie.imDbRating)")
+        NavigationView {
+            List(movies) { movie in
+                VStack {
+                    HStack {
+                        ImageView(withURL: movie.image)
+                        VStack(alignment: .leading) {
+                            Text(movie.fullTitle)
+                            Text("Rating: \(movie.imDbRating)")
+                        }
+                        Image("Icon-ChevronUpDown")
+                            .onTapGesture(perform: {
+                                withAnimation(.easeInOut(duration: 0.5)) {
+                                    tapped.toggle()
+                                }
+                            })
+                    }
+
+                    if tapped {
+                        Text(movie.crew)
+                            .padding()
+                            .clipped()
+                    }
+                }
+            }.onAppear() {
+                Api().getMovies { (movies) in
+                    self.movies = movies
                 }
             }
-        }.onAppear() {
-            Api().getMovies { (movies) in
-                self.movies = movies
-            }
+            .navigationBarTitle(Text("Top Movies"))
         }
     }
 }
